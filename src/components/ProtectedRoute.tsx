89fe3838d962fd7911cac116abb8f488a0f69a0d
Roflochinsky <nikitatrubaev92@gmail.com>
2026-01-27 09:11:05 +0300

    Fix missing imports in ProtectedRoute

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,3 +1,6 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 import MainLayout from './layout/MainLayout';
 
 const ProtectedRoute = () => {
